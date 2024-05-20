a6ffb71929a2a80a61611970b49320c29b8e6796
fagner-as-fag <dev.fagnerpyja@gmail.com>
2024-05-20 13:37:24 -0300

    Update conexao.java

--- src/conexao/conexao.java
+++ src/conexao/conexao.java
@@ -1,2 +1,36 @@
-package conexao;public class conexao {
+package conexao;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class conexao {
+
+
+
+
+    public  String Conectar(){
+        var url = "https://v6.exchangerate-api.com/v6/4a2745f05a8ad45842e2458f/latest/USD";
+
+        try {
+            HttpClient buscador = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpResponse<String> response = buscador.send(request, HttpResponse.BodyHandlers.ofString());
+          return response.body();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+    }
+
+
+
+
 }
