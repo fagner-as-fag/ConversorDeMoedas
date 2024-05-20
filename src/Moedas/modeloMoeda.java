d869fda499c5bdcfe023a80e40b6b148476861a7
fagner-as-fag <dev.fagnerpyja@gmail.com>
2024-05-20 13:33:56 -0300

    Update modeloMoeda.java

--- src/Moedas/modeloMoeda.java
+++ src/Moedas/modeloMoeda.java
@@ -1,2 +1,8 @@
-package Moedas;public record modeloMoeda() {
+package Moedas;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Map;
+
+public record modeloMoeda(@SerializedName("conversion_rates") Map<String,String> conversion_rates) {
 }
