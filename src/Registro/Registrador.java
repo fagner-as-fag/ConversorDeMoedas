6c0af31c4db9c89c79ff34946e536018b7d22186
fagner-as-fag <dev.fagnerpyja@gmail.com>
2024-05-20 13:36:09 -0300

    Update Registrador.java

--- src/Registro/Registrador.java
+++ src/Registro/Registrador.java
@@ -1,2 +1,59 @@
-package Registro;public class Registrador {
+package Registro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Registrador {
+    List<Registro> RegistroDeConversões = new ArrayList<>();
+
+
+    public void Regisrar(int x, double y, double z) {
+
+        String m = null;
+
+        switch (x) {
+            case 1:
+                m = "Dolar =>> Peso Argentino";
+                break;
+            case 2:
+                m = "Peso Argentino =>> Dolar";
+                break;
+            case 3:
+                m = "Dolar =>> Real Brasileiro";
+                break;
+            case 4:
+                m = "Real Brasileiro =>> Dolar";
+            case 5:
+
+                m = "Dolar =>> Peso Colombiano";
+                break;
+            case 6:
+
+                m = "Peso Colombiano =>> Dolar";
+                break;
+            case 7:
+
+                m = "Dolar =>> Euro";
+                break;
+            case 8:
+                m = "Euro =>> Dolar";
+
+
+        }
+Registro registro= new Registro(m,y,z);
+        RegistroDeConversões.add(registro);
+
+
+
+    }
+
+public  void exibirRegistro(){
+
+        for ( Registro registro : this.RegistroDeConversões){
+            System.out.println("\n"+registro);
+
+        }
+}
+
+
 }
