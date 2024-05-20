849b392db049034c36966928674dd599b706eba1
fagner-as-fag <dev.fagnerpyja@gmail.com>
2024-05-20 13:35:31 -0300

    Update Registro.java

--- src/Registro/Registro.java
+++ src/Registro/Registro.java
@@ -1,2 +1,26 @@
-package Registro;public class Registro {
+package Registro;
+
+public class Registro {
+
+
+    private double valor;
+    private String tipoDeOperacao;
+    private double convertido;
+
+    @Override
+    public String toString() {
+        return "operação :"
+                + this.tipoDeOperacao +
+                "\nvalor inicial: " +
+                this.valor +
+                "\nvalor após conversão: " +
+                this.convertido
+                ;
+    }
+
+    public Registro(String m, double y, double z) {
+        this.valor = y;
+        this.tipoDeOperacao = m;
+        this.convertido = z;
+    }
 }
